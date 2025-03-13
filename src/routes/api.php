816ca1092c8f966b0b5744e90336e2c50a607523
Hanif-Ibadurrahman <irahman.hanif@gmail.com>
2025-03-13 22:53:40 +0700

    update code

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
+
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/user', function (Request $request) {
